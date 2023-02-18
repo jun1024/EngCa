e80c46a4ee6a2d13f4e783434274ec41850395c7
juni2335 <juni941024@gmail.com>
2023-02-18 23:24:38 +0900

    add wordcard

---
+++ lib/wordcard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WordCard extends StatefulWidget {
+  const WordCard({super.key});
+
+  @override
+  State<WordCard> createState() => _WordCardState();
+}
+
+class _WordCardState extends State<WordCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
